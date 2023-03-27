c8887f82440249dd866851bd529f83bea70fbf79
nfuoco <natalia_fuoco@hotmail.com>
2023-03-27 13:58:50 -0300

    caroulel added

--- src/app/components/carrousel/carrousel.component.ts
+++ src/app/components/carrousel/carrousel.component.ts
@@ -9,14 +9,30 @@ import { Component, OnInit,  EventEmitter, Output, Input } from '@angular/core';
 export class CarrouselComponent implements OnInit {
 
   @Input() id:string = '';
-  @Input() texto:string = '';
+  @Input() target:string = '';
+  @Input() carouselRide:string = '';
   @Input() carouselClass:string = '';
   @Input() carouselClass1:string = '';
+  @Input() carouselClass2:string = '';
+  @Input() slide1Title:string = '';
+  @Input() slide2Title:string = '';
+  @Input() slide3Title:string = '';
+
+  @Input() slide1Text:string = '';
+  @Input() slide2Text:string = '';
+  @Input() slide3Text:string = '';
+
+  @Input() slide1Img:string = '';
+  @Input() slide2Img:string = '';
+  @Input() slide3Img:string = '';
 
 
   constructor() { }
 
   ngOnInit(): void {
+console.log(this.target);
+
+
   }
 
 }
